4e024129c11f6eebc34cba5b62f1a06dc35ef130
Hemayil Cebrayilova <hemayil1556@gmail.com>
2025-09-30 22:40:17 -0700

    Added urls.py api auth admin

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def return_all_links(request):
-    return HttpResponse('<h2>API Links</h2><ul><li><a href="/api/books/">Books</a></li><li><a href="/api/authors/">Authors</a></li><li><a href="/api/copies/">Book Copies</a></li><li><a href="/auth/login/">Login</a></li><li><a href="/auth/register/">Register</a></li></ul>'
+    return HttpResponse('<h2>API Links</h2><ul><li><a href="/api/books/">Books</a></li><li><a href="/api/copies/">Book Copies</a></li><li><a href="/auth/login/">Login</a></li><li><a href="/auth/register/">Register</a></li></ul>'
     )
 
 urlpatterns = [
